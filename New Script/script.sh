5182777e209e0aa5167ca2ac550a4ec8ee458df8
Mahdi Qiamast <78082316+Qiamast@users.noreply.github.com>
2023-02-08 09:09:38 -0500

    Add basic structure that you can use
    This template provides a basic structure that you can use for any bash script you write. You can add or remove sections as needed based on the specific requirements of each script.

---
+++ New Script/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# A brief description of the script and what it does
+
+# Define any variables that the script will use
+
+# Perform any necessary checks or preparations before the main part of the script starts
+
+# Main part of the script: perform the desired actions
+
+# Provide any necessary clean up or post-processing steps
+
+# Exit the script with a status code, indicating success (0) or failure (non-zero)
+exit 0
